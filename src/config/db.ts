a63510e121d021313a4bcbe639f7117897ac6590
Magdy371 <magdyelshrief37@gmail.com>
2025-08-13 12:39:33 +0300

    add db.confg() to the entry point no needed in db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,7 +1,4 @@
 import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-
-dotenv.config();
 const connectDB = async():Promise<void>=>{
     try {
         const conn = await mongoose.connect(process.env.MONGODB_URI as string);
